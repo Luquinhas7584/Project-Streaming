49fc532286a32f37bd96b53de4b4e2a1320a39c9
Luquinhas7584 <110787175+Luquinhas7584@users.noreply.github.com>
2022-12-01 21:45:33 -0400

    Vue Vid
    Fail?

--- lvl-site/src/App.vue
+++ lvl-site/src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <nav id="menu-h">
+        <ul>
+            <li><a href="link">Inicio</a></li>
+            <li><a href="#">chora</a></li>
+            <li><a href="#">algo</a></li>
+            <li><a href="#">icons</a></li>
+            <li><a href="#">Donos</a></li>
+            <li><a id="tras" href="#">Login</a></li>
+        </ul>
+    </nav>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Exo:wght@100&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Exo:wght@100&family=Libre+Baskerville&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Exo:wght@100&family=Hind+Madurai:wght@300&family=Libre+Baskerville&display=swap');
+* {
+    margin: 0 auto;
+    padding: 0;
+    box-sizing: border-box;
 }
+/* IN Fundo principal*/
+
+/* FM Fundo principal*/
+
+/*IN Barra de Navegação*/
+
+
+
+#menu-h ul {
+    list-style: none;
+    padding: 0;
+
+}
+
+#menu-h ul li {
+    display: inline;
+}
+
+#menu-h ul li a {
+    color: blue;
+    display: inline-block;
+    font-family: 'Exo';
+  font-style: normal;
+  font-weight: 100;
+  font-display: swap;
+    padding: 20px;
+    text-decoration: none;
+    transition: background .6s;
+}
+
+#menu-h ul li a:hover {
+    background-color: #700B97;
+}
+
+#menu-h ul li:last-child a {
+    float: right;
+    background-color: #700B97;
+}
+/*FM Barra de Navegação*/
+/*IN Carrossel*/
+/*FM Carrossel*/
+/*IN Alter*/
+
+
+
+
+
 </style>
