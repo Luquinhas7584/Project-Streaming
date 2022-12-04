c76bc03c3ab88dacbf231d05704412063d3d7524
Luquinhas7584 <110787175+Luquinhas7584@users.noreply.github.com>
2022-12-03 20:57:16 -0400

    Evolution
    avanços

--- lvl-site/src/App.vue
+++ lvl-site/src/App.vue
@@ -1,75 +1,24 @@
 <template>
-   <nav id="menu-h">
-        <ul>
-            <li><a href="link">Inicio</a></li>
-            <li><a href="#">chora</a></li>
-            <li><a href="#">algo</a></li>
-            <li><a href="#">icons</a></li>
-            <li><a href="#">Donos</a></li>
-            <li><a id="tras" href="#">Login</a></li>
-        </ul>
-    </nav>
-    
+   <div>
+    <h1 class="nav">Naveg</h1>
+    <h2>My name is: {{ Nome }}</h2>
+   </div>
 </template>
-
+  
 <script>
-
+export default{
+    name:"Nav-Bar",
+    data(){
+        return{
+            Nome:"Lucas"
+        }
+    }
+}
 </script>
-
+  
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Exo:wght@100&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Exo:wght@100&family=Libre+Baskerville&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Exo:wght@100&family=Hind+Madurai:wght@300&family=Libre+Baskerville&display=swap');
-* {
-    margin: 0 auto;
-    padding: 0;
-    box-sizing: border-box;
-}
-/* IN Fundo principal*/
-
-/* FM Fundo principal*/
-
-/*IN Barra de Navegação*/
-
-
-
-#menu-h ul {
-    list-style: none;
-    padding: 0;
-
-}
-
-#menu-h ul li {
-    display: inline;
-}
-
-#menu-h ul li a {
-    color: blue;
-    display: inline-block;
-    font-family: 'Exo';
-  font-style: normal;
-  font-weight: 100;
-  font-display: swap;
-    padding: 20px;
-    text-decoration: none;
-    transition: background .6s;
-}
-
-#menu-h ul li a:hover {
-    background-color: #700B97;
-}
-
-#menu-h ul li:last-child a {
-    float: right;
-    background-color: #700B97;
-}
-/*FM Barra de Navegação*/
-/*IN Carrossel*/
-/*FM Carrossel*/
-/*IN Alter*/
-
-
-
-
-
+    .nav{
+        color: blue;
+    }
 </style>
+  
